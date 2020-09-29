257360aa08da6c41c0b1f2cfdb641b23734486af
Jeremy Wood <js.wood206@gmail.com>
2020-09-29 01:37:35 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,17 +6,19 @@ module Players
       # center = 5
       # corner = [1,3,7,9]
       # edge = [2,4,6,8]
+      optional_moves = []
       move_list.map do |x|
-        binding.pry
+        # binding.pry
         if !input.taken?(x)
-          x
+          optional_moves << x  
         end
       end
+      binding.pry
       #first priority is stop 3 in a row
       #selects center first
       #selects corner next
       #selects to finish row
-      x
+      optional_moves.first
     end
 
   end
