4c0bcad52485d4bcc25545d1847eb8c493868d57
Jeremy Wood <js.wood206@gmail.com>
2020-09-29 01:02:23 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,6 +2,8 @@ module Players
   class Computer < Player
 
     def move(input)
+      move_list = (1..9)
+      binding.pry
       #first priority is stop 3 in a row
       #selects center first
       #selects corner next
