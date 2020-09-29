09c001ec8b77ae9f90d8fab6b3b0afa9de947b3e
Jeremy Wood <js.wood206@gmail.com>
2020-09-29 01:31:08 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,9 +3,9 @@ module Players
 
     def move(input)
       move_list = [5,1,3,7,9,2,4,6,8]
-      center = 5
-      corner = [1,3,7,9]
-      edge = [2,4,6,8]
+      # center = 5
+      # corner = [1,3,7,9]
+      # edge = [2,4,6,8]
       move_list.map do |x|
         # binding.pry
         if !input.taken?(x)
@@ -16,7 +16,7 @@ module Players
       #selects center first
       #selects corner next
       #selects to finish row
-      #
+      x
     end
 
   end
