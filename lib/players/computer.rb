ee9c228fb278ec0fd89bd9c5e76240c125502ae3
Jeremy Wood <js.wood206@gmail.com>
2020-09-29 01:10:56 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,6 +3,13 @@ module Players
 
     def move(input)
       move_list = (1..9)
+      center = 5
+      corner = [1,3,7,9]
+      edge = [2,4,6,8]
+      binding.pry
+      if !input.taken?(5)
+        5
+      elsif input.taken?(corner.random)
       binding.pry
       #first priority is stop 3 in a row
       #selects center first
