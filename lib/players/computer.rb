aa8da54119fd6486f6fce595ce3ad99b5fbb8df9
Jeremy Wood <js.wood206@gmail.com>
2020-09-29 01:39:43 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,9 +3,6 @@ module Players
 
     def move(input)
       move_list = ["5","1","3","7","9","2","4","6","8"]
-      # center = 5
-      # corner = [1,3,7,9]
-      # edge = [2,4,6,8]
       optional_moves = []
       move_list.map do |x|
         if !input.taken?(x)
